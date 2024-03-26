e91222b13aeeb745e29558b5a9c058fd070ecdf6
Syed Sohaib <97386434+Sohaib0009@users.noreply.github.com>
2024-03-26 05:56:16 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,20 @@
+let itemContainerElement = document.querySelector('.cont');
+let innerHTML = '';
+item.forEach(item =>{
+    innerHTML += `<div class="item_container">
+    <a href="#"><img src="${item.item_img}" alt=""></a>
+    <div class="rating">
+       ${item.rating.stars} ★ | ${item.rating.review}
+    </div>
+    <div class="item_name">${item.item_name}</div>
+    <div class="price">
+        <span class="current_price">Rs ${item.current_price}</span><br>
+        <span class="original_price">Rs ${item.original_price}</span>
+        <span class="discount">(${item.discount}% off)</span>
+    </div>
+    <button class="btn_add_bag" onclick="addtoBag">Add to Bag</button>
+    </div>`
+})
+itemContainerElement.innerHTML = innerHTML;
+
+
